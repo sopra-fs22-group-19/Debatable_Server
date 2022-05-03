b98e0e7d9ee012ff1fdab89723bc86942d1cf887
chenfei ma <91160241+chenfeimauzh@users.noreply.github.com>
2022-05-03 12:12:34 +0200

    websocket endpoint config. test controller get message.

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/config/WebsocketConfig.java
@@ -0,0 +1,34 @@
+package ch.uzh.ifi.hase.soprafs22.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration
+@EnableWebSocketMessageBroker
+    public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // Set prefix for the endpoint that the client listens for our messages from
+        registry.enableSimpleBroker("/debateRoom");
+
+        // Set prefix for endpoints the client will send messages to
+        registry.setApplicationDestinationPrefixes("/debates");
+
+    }
+
+
+    //register STOMP endpoint
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // Registers the endpoint where the connection will take place
+        registry.addEndpoint("/ws-endpoint").withSockJS();
+    }
+}
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/controller/wsDebateRoomController.java
@@ -0,0 +1,45 @@
+package ch.uzh.ifi.hase.soprafs22.controller;
+
+import ch.uzh.ifi.hase.soprafs22.entity.DebateRoom;
+import ch.uzh.ifi.hase.soprafs22.entity.Intervention;
+import ch.uzh.ifi.hase.soprafs22.rest.dto.InterventionPostDTO;
+import ch.uzh.ifi.hase.soprafs22.rest.mapper.DTOMapper;
+import ch.uzh.ifi.hase.soprafs22.service.DebateService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.HtmlUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.security.Principal;
+
+@Controller
+public class wsDebateRoomController {
+
+    ///current post debates/rooms/{roomId}/msg
+    // Handles messages from /debates/rooms/{roomId}/msg. (The Spring adds the /debates prefix for us).
+    // Need to send to this url
+    @MessageMapping("/rooms/{roomId}/msg")
+    // Sends the return value of this method to /debateRoom/{roomId}
+    // Need to subscribe to this url
+    @SendTo("/debateRoom/{roomId}")
+    public String getMessages(InterventionPostDTO interventionPostDTO) {
+
+        String msg = interventionPostDTO.getMessageContent();
+
+        return msg;
+
+    }
+
+
+}
