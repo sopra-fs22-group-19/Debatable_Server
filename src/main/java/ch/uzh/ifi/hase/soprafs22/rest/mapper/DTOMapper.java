6d9b9216b8889c85d91bc436f2afde91658d839f
Juan Diego Bermeo <juandiego.bermeoortiz@uzh.ch>
2022-05-09 16:09:51 +0200

    fix some code smells

--- src/main/java/ch/uzh/ifi/hase/soprafs22/rest/mapper/DTOMapper.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/rest/mapper/DTOMapper.java
@@ -1,6 +1,5 @@
 package ch.uzh.ifi.hase.soprafs22.rest.mapper;
 
-import ch.uzh.ifi.hase.soprafs22.constant.DebateState;
 import ch.uzh.ifi.hase.soprafs22.entity.*;
 import ch.uzh.ifi.hase.soprafs22.rest.dto.*;
 import org.mapstruct.*;
