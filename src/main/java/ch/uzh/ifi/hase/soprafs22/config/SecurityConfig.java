da2a456e28ac246afcf40c5476cedbf87e531475
chenfei ma <91160241+chenfeimauzh@users.noreply.github.com>
2022-05-15 01:08:19 +0200

    block user profile access

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package ch.uzh.ifi.hase.soprafs22.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/","/users","/login","/users/guests","/users/1","/debates/**","/translate","/api/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
