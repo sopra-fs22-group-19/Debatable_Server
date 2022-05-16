d25fdb04bb5e5bbd99ad7a07dbfe59c2fe5eda36
chenfei ma <91160241+chenfeimauzh@users.noreply.github.com>
2022-05-16 19:03:54 +0200

    logout

--- src/main/java/ch/uzh/ifi/hase/soprafs22/config/SecurityConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/config/SecurityConfig.java
@@ -32,6 +32,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .logout().logoutUrl("/logout")
+                .and()
                 .authorizeRequests()
                 .antMatchers("/users/*").authenticated()
                 .anyRequest().permitAll()
