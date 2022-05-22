40fd5161c4f49b23aac2a26cfdc673e0217908c4
chenfei ma <91160241+chenfeimauzh@users.noreply.github.com>
2022-05-23 01:21:48 +0200

    only integration test left

--- src/main/java/ch/uzh/ifi/hase/soprafs22/config/SecurityConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/config/SecurityConfig.java
@@ -1,6 +1,11 @@
 package ch.uzh.ifi.hase.soprafs22.config;
 
+import ch.uzh.ifi.hase.soprafs22.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,23 +13,24 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsService userDetailsService;
 
 
+    private final UserService userService;
 
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
+    public SecurityConfig(UserService userService) {
+        this.userService = userService;
     }
 
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userService);
     }
 
     @Override
@@ -34,6 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout().logoutUrl("/logout")
                 .and()
                 .authorizeRequests()
+                //same function by remove the banner access from frontend
                 /*
                 .antMatchers(HttpMethod.PUT,"/users/*").hasAuthority("REGISTER")
                 .antMatchers(HttpMethod.DELETE,"/users/*").hasAuthority("REGISTER")
